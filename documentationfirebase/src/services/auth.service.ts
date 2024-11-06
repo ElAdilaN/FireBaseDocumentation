d6422340b5207de8547653c702eaf224cb07fae0
Noureddin El Adila <neladila@eio.cat>
2024-11-06 12:18:24 +0100

    add the login

--- documentationfirebase/src/services/auth.service.ts
+++ documentationfirebase/src/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth, private router: Router) {}
+
+  
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password).then(
+      (res: any) => {
+       //alert('registred succesfully');
+        this.router.navigate(['/dashboard']);
+      },
+      (err) => {
+        alert(err.message);
+
+        this.router.navigate(['/register']);
+      }
+    );
+  }
+
 }
