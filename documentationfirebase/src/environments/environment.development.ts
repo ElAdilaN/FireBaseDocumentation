dd275fa5af926f774c66442e3d6a992b1b5ef1e7
Noureddin El Adila <neladila@eio.cat>
2024-11-06 11:46:00 +0100

    add the configuration of the firabse

---
+++ documentationfirebase/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+      apiKey: 'AIzaSyBWQWsQOZyOOMOE9SZtsQHECB6Jhfdolmw',
+      authDomain: 'fir-documentation-923f0.firebaseapp.com',
+      projectId: 'fir-documentation-923f0',
+      storageBucket: 'fir-documentation-923f0.firebasestorage.app',
+      messagingSenderId: '442502400026',
+      appId: '1:442502400026:web:1ace627696b8db55df69a3',
+      measurementId: 'G-H1M7NEFE2P',
+    },
+  };
+  
--- documentationfirebase/src/main.ts
+++ documentationfirebase/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from './environments/environment.development';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+});
